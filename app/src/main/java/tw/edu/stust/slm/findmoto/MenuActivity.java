154ddaf756769a023c4bd3644a4c34342339fd2d
smallmentor <s92203aaa@gmail.com>
2018-10-13 17:31:55 +0800

    加入List系統前的預防性存檔 增加註解 改善程式碼

--- app/src/main/java/tw/edu/stust/slm/findmoto/MenuActivity.java
+++ app/src/main/java/tw/edu/stust/slm/findmoto/MenuActivity.java
@@ -8,15 +8,18 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class MenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     View startLayout,listLayout,settingLayout;
     Button btnGotoList_start,btnGotoList_setting,btnFind,btnStartCorrection;
+    ListView deviceList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,19 +27,46 @@ public class MenuActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //開始搜尋、裝置列表、矯正等 介面之引用
         startLayout = findViewById(R.id.contentStart);
         listLayout = findViewById(R.id.contentList);
         settingLayout = findViewById(R.id.contentSetting);
 
+        //前進裝置列表按鈕
         btnGotoList_start = findViewById(R.id.btnGotoList_start);
         btnGotoList_setting = findViewById(R.id.btnGotoList_setting);
+
         btnFind = findViewById(R.id.btnFind);
+
         btnStartCorrection = findViewById(R.id.btnStartCorrection);
 
-        btnGotoList_setting.setOnClickListener(btnClick);
-        btnGotoList_start.setOnClickListener(btnClick);
-        btnFind.setOnClickListener(btnClick);
-        btnStartCorrection.setOnClickListener(btnClick);
+        //裝置清單
+        deviceList = findViewById(R.id.deviceList);
+
+        //事件
+        btnGotoList_setting.setOnClickListener(btnGoToListClick);
+        btnGotoList_start.setOnClickListener(btnGoToListClick);
+
+        //開啟 activity_find 畫面
+        btnFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent();
+                it.setClass(MenuActivity.this,FindActivity.class);
+                startActivity(it);
+            }
+        });
+
+        //開啟 activity_correction 畫面
+        btnStartCorrection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent();
+                it.setClass(MenuActivity.this,CorrectionActivity.class);
+                startActivity(it);
+            }
+        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -47,6 +77,20 @@ public class MenuActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    private void goToList() {
+        startLayout.setVisibility(View.GONE);
+        listLayout.setVisibility(View.VISIBLE);
+        settingLayout.setVisibility(View.GONE);
+    }
+
+    View.OnClickListener btnGoToListClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            goToList();
+        }
+    };
+
+    //跟跳單有關的東西
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -57,22 +101,26 @@ public class MenuActivity extends AppCompatActivity
         }
     }
 
+    // 跳單事件
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // 跳單事件
+
         int id = item.getItemId();
 
         if (id == R.id.nav_Start) {
             startLayout.setVisibility(View.VISIBLE);
             listLayout.setVisibility(View.GONE);
             settingLayout.setVisibility(View.GONE);
+
         } else if (id == R.id.nav_list) {
             goToList();
+
         } else if (id == R.id.nav_setting) {
             startLayout.setVisibility(View.GONE);
             listLayout.setVisibility(View.GONE);
             settingLayout.setVisibility(View.VISIBLE);
+
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_end) {
@@ -83,30 +131,5 @@ public class MenuActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
-    private void goToList() {
-        startLayout.setVisibility(View.GONE);
-        listLayout.setVisibility(View.VISIBLE);
-        settingLayout.setVisibility(View.GONE);
-    }
-
-    View.OnClickListener btnClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-
-            Intent it = new Intent();
-
-            if(view==btnGotoList_setting || view==btnGotoList_start){
-                goToList();
-            }else if(view==btnFind){
-                it.setClass(MenuActivity.this,FindActivity.class);
-                startActivity(it);
-            }else if(view==btnStartCorrection) {
-                it.setClass(MenuActivity.this,CorrectionActivity.class);
-                startActivity(it);
-            }
-        }
-    };
-
 
 }
